8fd200d7575a2c27c75e739b44c5c217330b61bc
toriqultonu <toriqul@mymedicalhub.com>
2025-04-10 16:30:35 +0600

    started project

---
+++ springboot-kotlin/src/test/kotlin/org/example/springbootkotlin/SpringbootKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.springbootkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
